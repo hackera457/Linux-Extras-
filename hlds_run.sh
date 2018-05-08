3c8e776184c5a5e022a779ae071e0ebe66e99b52
hackera457 <hackera457@gmail.com>
2018-05-08 11:22:44 +0300

    Update hlds_run.sh

--- hlds_run.sh
+++ hlds_run.sh
@@ -1,127 +1,76 @@
- #!/bin/sh
-    ### BEGIN INIT INFO
-    # Provides:          csservers
-    # Required-Start:    $remote_fs $syslog
-    # Required-Stop:     $remote_fs $syslog
-    # Default-Start:     2 3 4 5
-    # Default-Stop:      0 1 6
-    # Short-Description: Start daemon at boot time
-    # Description:       Enable service provided by daemon.
-    ### END INIT INFO
-    # This script edit by kostov[Virtual.New.BG]
-    # Server options
-    TITLE='Dedicated Server'
-    SESSION='hlds'
-    DAEMON='hlds_run'
-    DIR='/home/hackera457/servers/d2'
-    USER='root'
+ #!/bin/bash
 
-    # Game options
-    IP='192.168.0.170'
-    PORT='27015'
-    MAP='de_dust2'
-    GAME='cstrike'
-    PLAYERS='32'
-    TICRATE='1200'
-    PINGBOOST='3'
-    SVLAN='0'
-    RCON=''
+# Script Settings
+NAME='d2'
+DESC='Dust2 Test Server'
+HLDS_PATH='/home/hackera457/servers/d2'
+CS_USER=root
+DIR='/home/hackera457/servers/d2'
+DAEMON=hlds_run
+PATH=/bin:/usr/bin:/sbin:/usr/sbin
 
-    # Server options string
-    OPTS="-game $GAME +ip $IP +maxplayers $PLAYERS +map $MAP -port $PORT +sv_lan $SVLAN +rcon_password $RCON +sys_ticrate $TICRATE -pingboost $PINGBOOST -master  -noipx -nojoy +log off -pidfile $DIR/$GAME/$SESSION.pid"
+# Server Settings
+IP='192.168.0.170'
+PORT='27015'
+MAP='de_dust2'
+GAME='cstrike'
+PLAYERS='32'
+TICRATE='1200'
+PINGBOOST='3'
+SVLAN='0'
+RCON=''
 
-    # Screen command
-    CURRENT_USER=$(/usr/bin/whoami)
-    if [ "$CURRENT_USER" = "$USER" ]; then
-        INTERFACE="/usr/bin/screen -A -m -d -S $SESSION"
-      else
-        INTERFACE="sudo -u $USER /usr/bin/screen -A -m -d -S $SESSION"
+# Server options string
+OPT = '-game $GAME +ip $IP +maxplayers $PLAYERS +map $MAP -port $PORT +sv_lan $SVLAN +rcon_password $RCON +sys_ticrate $TICRATE -pingboost $PINGBOOST -master  -noipx -nojoy +log off'
+
+case "$1" in
+ start)
+    if [ ! -z "$(pidof ./hlds_linux)" ]
+       then
+       echo "HLDS is already running!"
+    else 
+       echo "Starting $DESC: $NAME"
+       su $CS_USER -c "cd $DIR; screen -m -d -S $NAME ./$DAEMON $OPT"
     fi
-
-    service_start() {
-        if [ -f $DIR/$GAME/$SESSION.pid ] || [ -f $DIR/$GAME/$SESSION-screen.pid ]; then
-            if [ "$(ps -p `cat $DIR/$GAME/$SESSION.pid` | wc -l)" -gt 1 ]; then
-                echo -e "Cannot start $TITLE.  Server is already running."
-            else
-                if [ "$(ps -p `cat $DIR/$GAME/$SESSION.pid` | wc -l)" -gt 1 ]; then
-                    kill -9 `cat $DIR/$GAME/$SESSION-screen.pid`
-                    echo "Killing process ID $id"
-                    echo "Removing $TITLE screen pid file"
-                    rm -rf $DIR/$GAME/$SESSION-screen.pid
-       break
-                fi
-       if [ -f $DIR/$GAME/$SESSION-screen.pid ]; then
-       rm -rf $DIR/$GAME/$SESSION-screen.pid
-       fi
-            echo "Removing $TITLE pid file"
-            rm -rf $DIR/$GAME/$SESSION.pid
-            screen -wipe 1> /dev/null 2> /dev/null
-            service_start
-            fi
-        else
-            if [ -x $DIR/$DAEMON ]; then
-                echo "Starting $TITLE"
-                cd $DIR
-                $INTERFACE $DIR/$DAEMON $OPTS
-                sleep 15
-                ps -ef | grep SCREEN | grep "$SESSION" | grep -v grep | awk '{ print $2}' > $DIR/$GAME/$SESSION-screen.pid
-                echo "$TITLE screen process ID written to $DIR/$GAME/$SESSION-screen.pid"
-                echo "$TITLE server process ID written to $DIR/$GAME/$SESSION.pid"
-               
-                echo "$TITLE started."
-                chmod 666 $DIR/$GAME/*.pid #1> /dev/null 2> /dev/null
-                chown $USER $DIR/$GAME/*.pid #1> /dev/null 2> /dev/null
-                sleep 15
-            fi
-        fi
-    }
-
-    service_stop() {
-        if [ -f $DIR/$GAME/$SESSION.pid ] || [ -f $DIR/$GAME/$SESSION-screen.pid ]; then
-            echo "Stopping $TITLE"
-            for id in `cat $DIR/$GAME/$SESSION-screen.pid`
-                do kill -9 $id
-                echo "Killing process ID $id"
-                echo "Removing $TITLE screen pid file"
-                rm -rf $DIR/$GAME/$SESSION-screen.pid
-                break
-            done
-            echo "Removing $TITLE pid file"
-            rm -rf $DIR/$GAME/$SESSION.pid
-            screen -wipe 1> /dev/null 2> /dev/null
-            echo "$TITLE stopped."
-        else
-            echo -e "Cannot stop $TITLE. Server is not running."
-        fi   
-    }
+    ;;
+ 
+ stop)
+    if [ ! -z "$(pidof ./hlds_linux)" ]
+       then
+       echo -n "Stopping $DESC: $NAME"
+       kill `ps aux | grep -v grep | grep -i $CS_USER | grep -i screen | grep -i $NAME | awk '{print $2}'`
+       echo " ... done."
+    else
+       echo "Coulnd't find a running $DESC"
+    fi
+    ;;
+ 
+ restart)
+    if [ ! -z "$(pidof ./hlds_linux)" ]
+       then
+       echo -n "Stopping $DESC: $NAME"
+       kill `ps aux | grep -v grep | grep -i $CS_USER | grep -i screen | grep -i $NAME | awk '{print $2}'`
+       echo " ... done."
+    else
+       echo "Coulnd't find a running $DESC"
+    fi
+ 
+    echo -n "Starting $DESC: $NAME"
+    su $CS_USER -c "cd $DIR; screen -m -d -S $NAME ./$DAEMON $OPT"
+    echo " ... done."
+    ;;
+ 
+ status)
+    ps aux | grep -v grep | grep hlds_r > /dev/null
+    CHECK=$?
+    [ $CHECK -eq 0 ] && echo "HLDS is UP" || echo "HLDS is DOWN"
+    ;; 
+ *)
+    echo "Usage: $0 {start|stop|status|restart}"
+    exit 1
+    ;;
+esac
+ 
+exit 0
 
 
-    service_watch() {
-            if [ `screen -wipe | grep $SESSION | grep -v grep | awk '{ print $2 }'` == '(Attached)' ]; then
-                    echo -e "Someone is already attached to the console of the server.\n Might want to check who"
-            else
-                    screen -r $SESSION
-            fi
-    }
-
-
-    case "$1" in
-        'start')
-            service_start
-            ;;
-        'stop')
-            service_stop
-            ;;
-        'restart')
-            service_stop
-            sleep 10
-            service_start
-            ;;
-       'watch')
-          service_watch
-          ;;
-        *)
-            echo "Usage $0 start|stop|restart|watch"
-    esac
-
-    exit 0
